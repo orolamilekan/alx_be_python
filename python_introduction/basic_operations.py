519a8c0f086e1a20e512cc07f321b4f3bbdcad6a
Orolamilekan <Orolamilekan@gmail.com>
2025-09-11 09:44:02 +0100

    Print added

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -5,3 +5,8 @@ sumAnswer = number1 + number2;
 subtractionAnswer = number1 - number2;
 multiplicationAnswer = number1 * number2;
 divisionAnswer = number1 / number2;
+
+print("Sum of number 1 and 2=:",sumAnswer);
+print("Subtraction of number 1 and 2=:",subtractionAnswer);
+print("Multiplication of number 1 and 2=:", multiplicationAnswer);
+print("Division of number 1 and 2=:", divisionAnswer);
